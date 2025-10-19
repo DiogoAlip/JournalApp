7008a24427a2f8b33e2df86aac0bfb9ade59e1c9
DiogoAlip <diogoalipazaga@gmail.com>
2025-10-18 19:34:34 -0500

    chore: archives exclude for coverage

--- vite.config.ts
+++ vite.config.ts
@@ -9,5 +9,22 @@ export default defineConfig({
     globals: true,
     environment: "jsdom",
     setupFiles: "./src/vitest.ts",
+    coverage: {
+      exclude: [
+        "src/journal/components/*",
+        "src/auth/routes/*",
+        "src/firebase/*",
+        "src/hooks/*",
+        "src/router/*",
+        "src/theme/*",
+        "src/ui/*",
+        "src/vite-env.d.ts",
+        "src/vitest.ts",
+        "vite.config.ts",
+        "eslint.config.js",
+        "**/index.ts*",
+        "**/thunks.ts",
+      ],
+    },
   },
 });
